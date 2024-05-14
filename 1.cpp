676d12ba7e594c9e3cec97cfbb2b9bbe3334ad07
dakshu04 <dakshgrows@gmail.com>
2024-05-14 13:24:05 +0530

    Added sepcifiers

--- 1.cpp
+++ 1.cpp
@@ -2,14 +2,25 @@
 using namespace std;
 
 class Hero {
-    //properties
-    int health;
+    
+    // properties
+    public:
+    int health; // if class is empty then also give 1 byte because of identification
+    char level; 
+    void print(){
+        cout<< level <<endl;
+    }
 };
 
 int main(){
-    //creation of object
-    Hero h1;
+    // creation of object
+    Hero ramesh;
 
-    cout<<"size : "<<sizeof(h1)<<endl;
+    cout << "Health of Ramesh is " << ramesh.health << endl;
+    cout << "Level of Ramesh is " << ramesh.level << endl;
+    
+   
+    // cout << "size: " << sizeof(ramesh) << endl;
+
     return 0;
 }
